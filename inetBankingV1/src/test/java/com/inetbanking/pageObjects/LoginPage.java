43d1505d7e80d8bba127ee2ed60d5767ce92d6ef
seema6104 <54417967+seema6104@users.noreply.github.com>
2020-07-27 15:15:26 -0400

    Trying to edit

--- inetBankingV1/src/test/java/com/inetbanking/pageObjects/LoginPage.java
+++ inetBankingV1/src/test/java/com/inetbanking/pageObjects/LoginPage.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+
 public class LoginPage {
 
 	   WebDriver Idriver;
